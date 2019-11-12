358df0932be6969e33a5f74f3adf1951f555f785
Ankush Sharma <ankushr46601@gmail.com>
2019-11-12 23:29:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region	= "ap-south-1"
-  access_key = "********"
-  secret_key = "********"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
+  region = "${var.AWS_REGION}"
 }
+
