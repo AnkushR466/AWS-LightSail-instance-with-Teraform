0abac4e9bdbfcc63a5641fe366d6a21e6088c4e4
Ankush Sharma <ankushr46601@gmail.com>
2019-11-12 23:31:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/bin/python
 
 from python_terraform import *
-tf = Terraform(working_dir='/home/ja/terraform/demo-3', variables={'count':enter})
+tf = Terraform(working_dir='/root/Desktop/terraform/Demo-1')
 tf.plan(no_color=IsFlagged, refresh=False, capture_output=True)
 approve = {"auto-approve": True}
 print(tf.plan())
